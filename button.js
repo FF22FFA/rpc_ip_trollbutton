23120e640326231648f34c76afde857af599b797
FF22FFA <142903811+FF22FFA@users.noreply.github.com>
2024-07-04 22:06:18 -0400

    Delete button.js

--- button.js
+++
@@ -1,8 +0,0 @@
-const bigBlueButton = document.getElementById('big-blue-button');
-
-bigBlueButton.addEventListener('click', () => {
-    bigBlueButton.style.display = 'none'; // hide the button
-    console.log('Button clicked!');
-    getip(); // Call the getip function from getip.js
-    playskibidi(); // Call the playskibidi function from playskibidi.js
-});
